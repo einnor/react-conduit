93d77ab4add6b1299342ce08a1ea0bbb9822f97f
Ronnie Nyaga <ronnienyaga@gmail.com>
2018-01-31 15:00:30 +0300

    Added Home component

---
+++ src/components/Home/index.js
@@ -0,0 +1,28 @@
+import Banner from './Banner';
+import MainView from './MainView';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+const mapStateToProps = state => ({
+  appName: state.appName
+});
+
+class Home extends Component {
+  render() {
+    return(
+      <div className="home-page">
+        <div className="row">
+          <MainView />
+
+          <div className="col-md-3">
+            <div className="sidebar">
+              <p>Popular Tags</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default connect(mapStateToProps, () => ({}))(Home);
