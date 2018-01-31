9ce22d01c1080affc85c0d11af2923f61bd9e4d7
Ronnie Nyaga <ronnienyaga@gmail.com>
2018-01-31 14:25:09 +0300

    Created Header component

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+'use strict';
+
+import React, { Component} from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <nav className="navbar navbar-light">
+        <div className="header">
+          <a className="navbar-brand">
+            { this.props.appName.toLowerCase() }
+          </a>
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default Header;
