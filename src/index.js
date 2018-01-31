c4568adbfdb8bb3bf4779fd2510267f80ba791e1
Ronnie Nyaga <ronnienyaga@gmail.com>
2018-01-31 14:03:32 +0300

    Setting up react-redux library

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import ReactDOM from 'react-dom';
+import React from 'react';
+import './index.css';
+import { createStore } from 'redux';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const defaultState = {
+  appName: 'React Conduit',
+  articles: null
+};
+
+const reducer = function(state = defaultState, action) {
+  return state;
+};
+
+const store = createStore(reducer);
+
+ReactDOM.render((
+  <Provider store={ store }>
+    <App />
+  </Provider>
+), document.getElementById('root'));
 registerServiceWorker();
