3f190f2678b3e2124ce1a573821d277caaaa8af2
Ronnie Nyaga <ronnienyaga@gmail.com>
2018-02-04 12:40:05 +0300

    Save user object and token to local storage on login and redirect to home

--- src/components/App.js
+++ src/components/App.js
@@ -3,25 +3,51 @@ import Home from './Home';
 import Login from './Login';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Router, Route } from 'react-router-dom';
+import agent from '../agent';
+import history from '../history';
 
 const mapStateToProps = state => ({
-  appName: state.common.appName
+  appName: state.common.appName,
+  currentUser: state.common.currentUser,
+  redirectTo: state.common.redirectTo
+});
+
+const mapDispatchToProps = dispatch => ({
+  onLoad: (payload, token) => dispatch({ type: 'APP_LOAD', payload, token }),
+  onRedirect: () => dispatch({ type: 'REDIRECT' })
 });
 
 class App extends Component {
+
+  componentWillMount() {
+    const token = window.localStorage.getItem('jwt');
+    if(token) {
+      agent.setToken(token);
+    }
+
+    this.props.onLoad(token ? agent.Auth.current() : null, token);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if(nextProps.redirectTo) {
+      history.replace(nextProps.redirectTo);
+      this.props.onRedirect();
+    }
+  }
+
   render() {
     return (
-      <Router>
+      <Router history={ history }>
         <div>
           <Header appName={ this.props.appName } />
 
           <Route exact path="/" component={ Home } />
-          <Route path="/login" component={ Login } />
+          <Route exact path="/login" component={ Login } />
         </div>
       </Router>
     );
   }
 }
 
-export default connect(mapStateToProps, () => ({})) (App);
+export default connect(mapStateToProps, mapDispatchToProps) (App);
